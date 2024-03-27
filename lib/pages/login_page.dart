ad888eb047cdfe1464aff81eaba692a3ac7c1a53
Himash <himash.20210756@iit.ac.lk>
2024-03-27 14:17:38 +0530

    Initial Commit + authentication

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,117 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:social_media_application/components/button.dart';
+import 'package:social_media_application/components/text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  final Function()? onTap;
+  const LoginPage ({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //text editing controllers
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  // sign user in
+  void signin() async{
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailTextController.text,
+        password: passwordTextController.text,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // logo
+                const Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+
+                const SizedBox(height: 50),
+
+                // welcome back message
+                Text(
+                  "Welcome back, you've been missed",
+                  style: TextStyle(
+                  color: Colors.grey[700],
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // email textfield
+                MyTextField(
+                  controller: emailTextController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 25),
+
+                // password textfield
+                MyTextField(
+                  controller: passwordTextController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 25),
+
+                // sign in button
+                MyButton(
+                    onTap: signin,
+                    text: 'Sign in',
+                ),
+
+                const SizedBox(height: 25),
+
+                // go to register page
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Not a member?',
+                      style: TextStyle(
+                      color: Colors.grey[700],
+                      ),
+                    ),
+
+                    const SizedBox(width: 4),
+
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                      'Register now',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
